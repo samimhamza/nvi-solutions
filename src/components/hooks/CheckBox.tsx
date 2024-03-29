5f60d39fd7fb5cf40a546ea640d32fb823c3aa7b
samimhamza <samim.hamza7@gmail.com>
2024-03-30 01:38:03 +0430

    graph partialy completed

--- src/components/hooks/CheckBox.tsx
+++ src/components/hooks/CheckBox.tsx
@@ -1,40 +1,39 @@
-import { Currency, useCheckedCurrencies } from '../../context/DataContext';
-import checkImage from './../../assets/check.png';
+import { Currency, useCheckedCurrencies } from "../../context/DataContext";
+import checkImage from "./../../assets/check.png";
 
 interface CheckboxProps {
-  label: string;
-  currency: Currency;
+	label: string;
+	currency: Currency;
 }
 
 function Checkbox({ label, currency }: CheckboxProps) {
-  const { checkedCurrencies, addCurrency, removeCurrency} =
-    useCheckedCurrencies();
+	const { checkedCurrencies, addCurrency, removeCurrency } =
+		useCheckedCurrencies();
 
-  const checked = checkedCurrencies.includes(currency); 
+	const checked = checkedCurrencies.includes(currency);
 
-  // Function to handle checkbox change
-  const handleCheckboxChange = () => {
-    if (checked) {
-      removeCurrency(currency);
-    } else {
-      addCurrency(currency); 
-    }
-  };
+	// Function to handle checkbox change
+	const handleCheckboxChange = () => {
+		if (checked) {
+			removeCurrency(currency);
+		} else {
+			addCurrency(currency);
+		}
+	};
 
-
-  return (
-    <div className='row'>
-      <label>
-        <input
-          type='checkbox'
-          checked={checked}
-          onChange={handleCheckboxChange}
-        />
-        <span>{checked && <img src={checkImage} width='50px' />}</span>
-      </label>
-      <p className='currency-code'>{label}</p>
-    </div>
-  );
+	return (
+		<div className="row">
+			<label>
+				<input
+					type="checkbox"
+					checked={checked}
+					onChange={handleCheckboxChange}
+				/>
+				<span>{checked && <img src={checkImage} width="47px" />}</span>
+			</label>
+			<p className="currency-code">{label}</p>
+		</div>
+	);
 }
 
 export default Checkbox;
--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,18 +1,18 @@
-import React, { ReactNode, createContext, useContext, useState } from 'react';
+import React, { ReactNode, createContext, useContext, useState } from "react";
 
 // Define the type for currency
-export type Currency = 'eur' | 'usd' | 'cny' | 'rub';
+export type Currency = "eur" | "usd" | "cny";
 export type DateIn = string;
 
 // Define the type for context
 interface CheckedCurrenciesContextType {
-  checkedCurrencies: Currency[];
-  addCurrency: (currency: Currency) => void;
-  removeCurrency: (currency: Currency) => void;
-  setStartDateInterval: string;
-  setEndDateInterval: string;
-  startDate: string;
-  endDate: string;
+	checkedCurrencies: Currency[];
+	addCurrency: (currency: Currency) => void;
+	removeCurrency: (currency: Currency) => void;
+	setStartDateInterval: string;
+	setEndDateInterval: string;
+	startDate: string;
+	endDate: string;
 }
 
 // Date
@@ -21,55 +21,58 @@ const sevenDaysAgo: Date = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
 const formattedToday: string = formatDate(today);
 const formattedSevenDaysAgo: string = formatDate(sevenDaysAgo);
 function formatDate(date: Date): string {
-  return date.toISOString().slice(0, 10);
+	return date.toISOString().slice(0, 10);
 }
 
 // Create a context to manage the state of checked currencies
 const CheckedCurrenciesContext = createContext<CheckedCurrenciesContextType>({
-  addCurrency: () => {},
-  removeCurrency: () => {},
-  checkedCurrencies: ['rub', 'eur', 'usd', 'cny'],
-  startDate: formattedSevenDaysAgo,
-  endDate: formattedToday,
-  setStartDateInterval: '',
-  setEndDateInterval: '',
+	addCurrency: () => {},
+	removeCurrency: () => {},
+	checkedCurrencies: ["eur", "usd", "cny"],
+	startDate: formattedSevenDaysAgo,
+	endDate: formattedToday,
+	setStartDateInterval: "",
+	setEndDateInterval: "",
 });
 
 // Provider component to wrap your application and provide the context
 export const CheckedCurrenciesProvider: React.FC<{ children: ReactNode }> = ({
-  children,
+	children,
 }) => {
-  const [checkedCurrencies, setCheckedCurrencies] = useState<Currency[]>([]);
+	const [checkedCurrencies, setCheckedCurrencies] = useState<Currency[]>([
+		"eur",
+		"usd",
+		"cny",
+	]);
 
+	// Function to add a currency to the checkedCurrencies array
+	const addCurrency = (currency: Currency) => {
+		setCheckedCurrencies([...checkedCurrencies, currency]);
+	};
 
-  // Function to add a currency to the checkedCurrencies array
-  const addCurrency = (currency: Currency) => {
-    setCheckedCurrencies([...checkedCurrencies, currency]);
-  };
+	// Function to remove a currency from the checkedCurrencies array
+	const removeCurrency = (currency: Currency) => {
+		setCheckedCurrencies(checkedCurrencies.filter((c) => c !== currency));
+	};
 
-  // Function to remove a currency from the checkedCurrencies array
-  const removeCurrency = (currency: Currency) => {
-    setCheckedCurrencies(checkedCurrencies.filter((c) => c !== currency));
-  };
-
-  return (
-    <CheckedCurrenciesContext.Provider
-      value={{
-        checkedCurrencies,
-        addCurrency,
-        removeCurrency,
-        startDate: formattedSevenDaysAgo,
-        endDate: formattedToday,
-        setStartDateInterval: '',
-        setEndDateInterval: '',
-      }}
-    >
-      {children}
-    </CheckedCurrenciesContext.Provider>
-  );
+	return (
+		<CheckedCurrenciesContext.Provider
+			value={{
+				checkedCurrencies,
+				addCurrency,
+				removeCurrency,
+				startDate: formattedSevenDaysAgo,
+				endDate: formattedToday,
+				setStartDateInterval: "",
+				setEndDateInterval: "",
+			}}
+		>
+			{children}
+		</CheckedCurrenciesContext.Provider>
+	);
 };
 
 // Custom hook to consume the CheckedCurrenciesContext
 export const useCheckedCurrencies = (): CheckedCurrenciesContextType => {
-  return useContext(CheckedCurrenciesContext);
+	return useContext(CheckedCurrenciesContext);
 };
